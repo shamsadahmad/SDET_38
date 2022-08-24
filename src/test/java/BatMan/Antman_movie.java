9f9f4f468d64c20ee9beea4f03be4c80c3eabbf6
Shamshad <Shamshad@DESKTOP-BIEA88R>
2022-08-24 12:58:52 +0530

    some thing is updated

---
+++ src/test/java/BatMan/Antman_movie.java
@@ -0,0 +1,10 @@
+package BatMan;
+
+public class Antman_movie {
+
+	public static void main(String[] args) {
+
+		System.out.println("this movies is on based on ant life");
+	}
+
+}
---
+++ src/test/java/BatMan/SuperMan.java
@@ -0,0 +1,9 @@
+package BatMan;
+
+public class SuperMan {
+
+	public static void main(String[] args) {
+		System.out.println("Hii this new Updates");
+	}
+
+}
