692255118909593ac1ca0c71dae8598e47a9aebf
shamsadahmad <103255259+shamsadahmad@users.noreply.github.com>
2022-08-29 21:57:15 +0530

    Update AntMan Script
    Success.

--- src/test/java/BatMan/Antman_movie.java
+++ src/test/java/BatMan/Antman_movie.java
@@ -5,6 +5,7 @@ public class Antman_movie {
 	public static void main(String[] args) {
 
 		System.out.println("this movies is on based on ant life");
+		System.out.println("this movies is Fantastic movie");
 	}
 
 }
